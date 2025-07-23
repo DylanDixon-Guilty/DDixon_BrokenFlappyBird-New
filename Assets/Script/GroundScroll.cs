3d9c22ad4a31e6a8aee07f0ed5ea82be135260a7
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-07-23 13:49:47 -0500

    Change the code in Ground Scroll to make the floor change more seamless. Also removed "public float resetPositionX;  public float startPositionX;" as they are no longer needed.

--- Assets/Script/GroundScroll.cs
+++ Assets/Script/GroundScroll.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class GroundScroll : MonoBehaviour
 {
     public float scrollSpeed = 2f;
-    public float resetPositionX; 
-    public float startPositionX;
+    //public float resetPositionX; 
+    //public float startPositionX;
+
+    private BoxCollider2D groundCollider;
+    private float groundPositionX;
+
+    private void Start()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundPositionX = groundCollider.size.x;
+    }
 
     void Update()
     {
@@ -13,10 +22,15 @@ public class GroundScroll : MonoBehaviour
             transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
         }
 
-        if (transform.position.x <= resetPositionX)
+        if (transform.position.x <= -groundPositionX)
         {
-            Vector2 newPos = new Vector2(startPositionX, transform.position.y);
-            transform.position = newPos;
+            RepositionGround();
         }
+    }
+
+    private void RepositionGround()
+    {
+        Vector2 newPos = new Vector2(groundPositionX * 2f, 0);
+        transform.position = (Vector2)transform.position + newPos;
     }
 }
